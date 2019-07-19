02acebfaece78ca12f7a6104fce81574bfcc0d0c
Luchao Qi <lc.qi@outloook.com>
2019-07-19 01:03:49 -0400

    Gouped data function

--- dataframe by manipulation.R
+++ dataframe by manipulation.R
@@ -1,3 +1,14 @@
+
+# grouped data frame to list
+WEEKdata = split(MINdata[,MINnames],MINdata$SEQN) %>% lapply(FUN = function(i){
+  a = rowSums(i)
+  data.frame(WeekLengths = length(rownames(i)),Mean = mean(a), Sd = sd(a), Max = max(a), Min = min(a))
+}) %>% 
+  do.call(what = "rbind")
+
+
+
+###############################
 dtClass <- dtTitanic[, list(n=sum(Freq)), by=c('Survived',
                                                'Class')]
 dtClass[, percentage := n / sum(n), by='Class']
