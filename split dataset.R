be3b3775933cd30b6fdc76b3dfe9cb5032c0e386
Luchao Qi <lc.qi@outloook.com>
2019-08-17 15:32:14 -0400

    lc

--- split dataset.R
+++ split dataset.R
@@ -1,3 +1,17 @@
+set.seed(1)
+samp <- sample(nrow(iris), nrow(iris)*0.75)
+iris.train <- iris[samp,]
+iris.valid <- iris[-samp,]
+
+
+
+
+
+#######################################################################################
+
+
+
+
 trainIdx = sample(c(TRUE, FALSE), length(y), replace = TRUE, prob = c(.7, .3))
 ytrain = y[trainIdx]
 xtrain = x[trainIdx, ] %>% scale()
