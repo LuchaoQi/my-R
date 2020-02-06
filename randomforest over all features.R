e93e79f2696b609c0a86cbf6d511720a8cd65b84
Luchao Qi <lc.qi@outlook.com>
2020-02-06 16:59:26 -0500

    Luchao's auto-commitment using shell script

---
+++ randomforest over all features.R
@@ -0,0 +1,7 @@
+library(randomForest)
+lapply(colnames(data), function(i){
+  fml = paste0(i,"~.-",i) %>% as.formula()
+  data.rf = randomForest(fml, data, importance = TRUE)
+  varImpPlot(data.rf,
+             main = i)
+})
