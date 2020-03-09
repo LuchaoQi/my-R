ba369d9594f3fd55f4d983aefb19ae12340593fa
Luchao Qi <lc.qi@outlook.com>
2020-03-09 15:25:11 -0400

    Luchao's auto-commitment using shell script

--- corrplot.R
+++ corrplot.R
@@ -7,3 +7,15 @@ corrplot(cor(data),
          order = 'hclust',
          p.mat = cor.mtest(data, conf.level = .95)$p
 )
+
+
+library(ggcorrplot)
+vec = list('data22','data26','data31','data36')
+lapply(vec, function(i){
+  data = get(i) %>% select(-rawid,-Age,-Gender)
+  ggcorrplot(cor(data, method = 'spearman'), 
+             # hc.order = TRUE,
+             type = "lower",
+             p.mat = cor_pmat(data),
+             title = i)
+})
